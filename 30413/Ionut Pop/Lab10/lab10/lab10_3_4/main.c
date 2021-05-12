d51196bf9f1a6e66a7fd98cb2d335e3957477318
Pop Ionut <ionutpop712@yahoo.com>
2021-05-12 20:06:32 +0300

    Lab10 Ionut Pop

---
+++ 30413/Ionut Pop/Lab10/lab10/lab10_3_4/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define LIMIT 100
+
+
+int a, b, n, v[LIMIT];
+
+bool multiple(){
+    int dif=abs()
+    for(int i=1;i<=n;++i){
+        if (a%v[i]==0){
+            printf("%d", a/v[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int main()
+{
+    scanf("%d%d%d", &a, &b, &n);
+    for (int i=1;i<=n;++i)
+        scanf("%d", &v[i]);
+
+    if (multiple())
+        return 0;
+
+
+
+
+
+    return 0;
+}
